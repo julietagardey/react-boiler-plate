1c09150a5438b9b2ac6dd541a4d17972139c1a03
julietagardey <jgardey@udesa.edu.ar>
2024-08-12 17:43:56 -0300

    creacion

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  
+  return (
+    <div>
+      <h1>Mi primera app en React</h1>
+      <img src="https://definicion.de/wp-content/uploads/2009/12/paisaje-1.jpg " alt="" />
+
+      </div>
+    
+  );
+}
+
+export default App;
